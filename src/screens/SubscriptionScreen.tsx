431b5ec6fb573cf1fa7109eaf19cfd691771d7da
Aleksandr Antciferov <wetterkrank@gmail.com>
2023-03-19 19:20:44 +0100

    add todo

--- src/screens/SubscriptionScreen.tsx
+++ src/screens/SubscriptionScreen.tsx
@@ -2,6 +2,13 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 import { SubscriptionScreenNavigationProps } from "../navigation/types";
 
+// Subscription parameters:
+// Destination (autocomplete)
+// Earliest departure date, latest return date (absolute or relative, like "tomorrow")
+// Min/max days at destination
+// Max stops
+// Inform me when the price drops below X / by X%
+
 export default function SubscriptionScreen({navigation}: SubscriptionScreenNavigationProps) {
   return (
     <View>
