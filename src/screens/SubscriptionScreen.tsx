e2bb11fea356a246be20d6902918badf69a44229
Aleksandr Antciferov <wetterkrank@gmail.com>
2023-03-21 23:55:50 +0100

    add datepicker

--- src/screens/SubscriptionScreen.tsx
+++ src/screens/SubscriptionScreen.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { StyleSheet, Switch, Text, View } from "react-native";
+
+import { Button, StyleSheet, Switch, Text, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { DestinationInput } from "../components/DestinationInput";
 
 import { SubscriptionScreenNavigationProps } from "../navigation/types";
@@ -16,23 +18,44 @@ export default function SubscriptionScreen({
 }: SubscriptionScreenNavigationProps) {
   // const [selectedItem, setSelectedItem] = useState({});
   const [relativeDatesEnabled, setRelativeDatesEnabled] = useState(false);
-  const toggleRelativeDates = (value: boolean) => setRelativeDatesEnabled(value);
+  const toggleRelativeDates = (value: boolean) =>
+    setRelativeDatesEnabled(value);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
 
   return (
     <View>
-
       <DestinationInput />
 
       <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={relativeDatesEnabled ? '#f5dd4b' : '#f4f3f4'}
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={relativeDatesEnabled ? "#f5dd4b" : "#f4f3f4"}
         ios_backgroundColor="#3e3e3e"
         onValueChange={toggleRelativeDates}
         value={relativeDatesEnabled}
       />
 
       <Text>Earliest departure date</Text>
-      <Text>Latest return date</Text>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </View>
   );
 }
