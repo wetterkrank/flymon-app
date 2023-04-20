2b65d8ce873b78a082ebb39006e7a626b2108ad6
Aleksandr Antciferov <wetterkrank@gmail.com>
2023-04-20 23:10:36 +0200

    add date formatting helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,3 +3,7 @@ import dayjs from "dayjs"
 export const formattedDate = (date: Date | null) => {
   return date ? dayjs(date).format("DD.MM.YYYY") : "none"
 }
+
+export const formattedApiDate = (date: Date | string) => {
+  return dayjs(date).format("DD/MM/YYYY")
+}
