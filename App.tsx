5bcd66bef5fa76ca5d3f50e05e961fb7f1a4214a
Aleksandr Antciferov <wetterkrank@gmail.com>
2023-05-29 21:45:33 +0200

    switch to custom dev client, add Auth0

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Auth0Provider } from "react-native-auth0";
 
 import HomeStackNavigator from "./src/navigation/HomeStack";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <HomeStackNavigator />
-    </NavigationContainer>
+    <Auth0Provider
+      domain={"dev-wttr.eu.auth0.com"}
+      clientId={"D9tpIq2LmI8ZqfiDm7ORaNXykiiA40Ff"}
+    >
+      <NavigationContainer>
+        <HomeStackNavigator />
+      </NavigationContainer>
+    </Auth0Provider>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,49 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import {useAuth0} from 'react-native-auth0';
 
 import { HomeScreenNavigationProps } from "../navigation/types";
 import { SubscriptionsList } from "../components/SubscriptionsList";
 import { useSubscriptions } from "../api/subscriptions/subscription";
 
 export default function HomeScreen({ navigation }: HomeScreenNavigationProps) {
+  const {authorize, clearSession, user, error: authError} = useAuth0();
+
+  const onLogin = async () => {
+    try {
+      await authorize({scope: 'openid profile email'}, {customScheme: 'com.wetterkrank.flymon'});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onLogout = async () => {
+    try {
+      await clearSession({customScheme: 'com.wetterkrank.flymon'});
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
+  const loggedIn = user !== undefined && user !== null;
+
   const { data, isLoading, error } = useSubscriptions();
 
   // TODO: separate existing and new subscription types? then we won't need undefined here
   const editSubscription = (id: string | undefined) => {
     id && navigation.navigate("Subscription", { subscriptionId: id });
   };
+
+
+  return (
+    <View style={styles.container}>
+      {loggedIn && <Text>You are logged in as {user.name}</Text>}
+      {!loggedIn && <Text>You are not logged in</Text>}
+      {error && <Text>{error.message}</Text>}
+
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+    </View>
+  );
 
   // TODO: better loading/error indication
   if (!data) {
