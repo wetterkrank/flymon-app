9bfb6e1b2752be288082c59f4d8aead1c9edc84e
Aleksandr Antciferov <wetterkrank@gmail.com>
2023-08-31 23:22:30 +0200

    upgrade expo, remove auth0

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { Auth0Provider } from "react-native-auth0";
-import * as Notifications from 'expo-notifications';
+import * as Notifications from "expo-notifications";
 
 import HomeStackNavigator from "./src/navigation/HomeStack";
 
@@ -14,13 +13,8 @@ export default function App() {
   });
 
   return (
-    <Auth0Provider
-      domain={"dev-wttr.eu.auth0.com"}
-      clientId={"D9tpIq2LmI8ZqfiDm7ORaNXykiiA40Ff"}
-    >
-      <NavigationContainer>
-        <HomeStackNavigator />
-      </NavigationContainer>
-    </Auth0Provider>
+    <NavigationContainer>
+      <HomeStackNavigator />
+    </NavigationContainer>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,33 +1,10 @@
-import { useEffect } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
-import { useAuth0 } from "react-native-auth0";
 
 import { HomeScreenNavigationProps } from "../navigation/types";
 import { SubscriptionsList } from "../components/SubscriptionsList";
 import { useSubscriptions } from "../api/subscriptions/subscription";
 
 export default function HomeScreen({ navigation }: HomeScreenNavigationProps) {
-  const { authorize, clearSession, user, error: authError } = useAuth0();
-
-  const onLogin = async () => {
-    try {
-      await authorize(
-        { scope: "openid profile email" },
-        { customScheme: "com.wetterkrank.flymon" }
-      );
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  const onLogout = async () => {
-    try {
-      await clearSession({ customScheme: "com.wetterkrank.flymon" });
-    } catch (e) {
-      console.log("Log out cancelled");
-    }
-  };
-  const loggedIn = user !== undefined && user !== null;
-
   const { data, isLoading, error: dataError } = useSubscriptions();
 
   // TODO: separate existing and new subscription types? then we won't need undefined here
@@ -43,14 +20,6 @@ export default function HomeScreen({ navigation }: HomeScreenNavigationProps) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.loginStatus}>
-        <Text>{loggedIn ? `You are logged in as ${user.name}` : 'You are not logged in'}</Text>
-        {authError && <Text>{authError.message}</Text>}
-        <Button
-          onPress={loggedIn ? onLogout : onLogin}
-          title={loggedIn ? "Log Out" : "Log In"}
-        />
-      </View>
 
       <View style={styles.destinations}>
         <SubscriptionsList data={data} handlePress={editSubscription} />
