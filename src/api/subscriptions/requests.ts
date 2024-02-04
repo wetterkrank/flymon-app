f2c6479adccd0c602c939237c7c72af18657f5ef
Aleksandr Antciferov <wetterkrank@gmail.com>
2024-02-04 20:40:02 +0100

    use staging server, fix fetcher

--- src/api/subscriptions/requests.ts
+++ src/api/subscriptions/requests.ts
@@ -1,23 +1,34 @@
 // TODO: possibly combine; see Kiwi Margarita
 // Maybe get rid of user in the path? If needed, request via query param
-const BASE_URL = "http://192.168.1.128:3000";
-const USER_ID = "0";
 
-export const fetcher = (path: string) => {
-  console.log(`API GET started: ${path}...`);
-  const result = fetch(`${BASE_URL}/users/${USER_ID}/${path}`).then((r) => r.json());
-  console.log("API GET done.");
-  return result;
-}
+// const BASE_URL = "http://192.168.1.128:3000";
+const BASE_URL = "https://flymon.yak.supplies";
+const USER_ID = "0";
+const HEADERS = {
+  "Content-Type": "application/json",
+  "X-Api-Key": "temporary-key",
+};
+
+export const fetcher = async (path: string) => {
+  console.log(`API GET started: ${path}`);
+  const response = await fetch(`${BASE_URL}/users/${USER_ID}/${path}`, {
+    headers: HEADERS,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    throw error;
+  }
+
+  return response.json();
+};
 
 export const stasher = async (path: string, method: string, body: string) => {
-  console.log(`API ${method} started: ${path}...`)
+  console.log(`API ${method} started: ${path}...`);
   const url = `${BASE_URL}/users/${USER_ID}/${path}`;
   return fetch(url, {
     method: method,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: HEADERS,
     body,
   });
 };
